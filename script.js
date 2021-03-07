8981528fe1f5db0da0c73f487db9b9171f2d2d58
iswara.chaitanya <iswara@sivananda.org>
2021-03-07 07:58:03 -0500

    move script to head

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-document.querySelectorAll('h3 + p').forEach($el => {
-  const p = $el.outerHTML;
-  const h3 = $el.previousElementSibling.outerHTML;
-  $el.previousElementSibling.outerHTML = `<div class="title-and-1st-para">${h3}${p}</div>`;
-  $el.remove();
-});
+//@ts-check
+document.addEventListener('DOMContentLoaded', () =>
+  // once the page loaded, wrap any combination of a
+  // header and paragraph in a div with class "title-and-1st-para"
+  document.querySelectorAll('h1 + p, h2 + p, h3 + p').forEach($el => {
+    const p = $el.outerHTML;
+    const h3 = $el.previousElementSibling.outerHTML;
+    $el.previousElementSibling.outerHTML = `<div class="title-and-1st-para">${h3}${p}</div>`;
+    $el.remove();
+  })
+);

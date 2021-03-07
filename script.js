4999827601dfa2249b471a1a2c117757c1fc8b7b
iswara.chaitanya <iswara@sivananda.org>
2021-03-07 12:53:22 -0500

    improve avoidance of page-break between headers and 1st paras

--- script.js
+++ script.js
@@ -2,10 +2,29 @@
 document.addEventListener('DOMContentLoaded', () =>
   // once the page loaded, wrap any combination of a
   // header and paragraph in a div with class "title-and-1st-para"
-  document.querySelectorAll('h1 + p, h2 + p, h3 + p').forEach($el => {
+  document.querySelectorAll(':is(h1,h2,h3) + p').forEach($el => {
+    console.log($el);
     const p = $el.outerHTML;
-    const h3 = $el.previousElementSibling.outerHTML;
-    $el.previousElementSibling.outerHTML = `<div class="title-and-1st-para">${h3}${p}</div>`;
-    $el.remove();
+    const headings = getHeadingPreviousSiblings($el).reverse();
+    console.log(headings);
+    const headingsHTMLs = headings.map(heading => heading.outerHTML);
+    headings.forEach(heading => heading.remove());
+    $el.outerHTML = `<div class="title-and-1st-para">${headingsHTMLs.join(
+      ''
+    )}${p}</div>`;
   })
 );
+
+// get an array of all adjacent headings before the paragraph.
+/**
+ * @param {Element} $el
+ */
+function getHeadingPreviousSiblings($el) {
+  const array = [];
+  let currentElement = $el.previousElementSibling;
+  while (currentElement?.tagName.match(/^H/)) {
+    array.push(currentElement);
+    currentElement = currentElement.previousElementSibling;
+  }
+  return array;
+}
